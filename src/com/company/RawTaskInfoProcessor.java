7a51e7e87ad4c56cdc85be67f50d3231aa53f43c
Tointer <Tointer@yandex.ru>
2020-10-11 06:35:23 +0300

    generator new filename

--- src/com/company/RawTaskInfoProcessor.java
+++ src/com/company/RawTaskInfoProcessor.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 public class RawTaskInfoProcessor {
 
     public TaskInfo Parse(String[] lines){
-        //String[] lines = rawString.split("\\r?\\n");
         if (lines.length < 4) throw  new RuntimeException();
         return new TaskInfo(lines[0], lines[1],  Arrays.copyOfRange(lines, 2, lines.length));
     }
